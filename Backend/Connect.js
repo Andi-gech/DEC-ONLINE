431adee1e2b35a46b4edfb444499f513b7482130
Andualem <114336578+Andi-gech@users.noreply.github.com>
2024-09-07 09:07:49 -0400

    mongodb connections

--- Backend/Connect.js
+++ Backend/Connect.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose')
 
 const connect=()=>{
   
-  return  mongoose.connect('mongodb://127.0.0.1:27017'
+  return  mongoose.connect('mongodb+srv://andifab23:fMUbqhDXIKRExCKq@cluster0.smzhf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 )
   
 }
